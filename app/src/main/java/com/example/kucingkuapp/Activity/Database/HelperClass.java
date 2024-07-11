8ae29458b1a83c41e00980a69fceea8cf724ceb5
Gabriel <genuardo21@gmail.com>
2024-07-11 10:10:51 +0700

    ini ngubah helper dedikit

--- app/src/main/java/com/example/kucingkuapp/Activity/Database/HelperClass.java
+++ app/src/main/java/com/example/kucingkuapp/Activity/Database/HelperClass.java
@@ -1,7 +1,12 @@
 package com.example.kucingkuapp.Activity.Database;
 
+import com.google.firebase.database.ServerValue;
+
 public class HelperClass {
-    private String name, email, password; // Ensure private access modifiers for encapsulation
+    private String name;
+    private String email;
+    private String password;
+    private Object timestamp; // Ensure private access modifiers for encapsulation
 
     public HelperClass() {
         // Default constructor required for calls to DataSnapshot.getValue(HelperClass.class)
@@ -11,6 +16,7 @@ public class HelperClass {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.timestamp = ServerValue.TIMESTAMP; // Initialize the timestamp
     }
 
     // Getters and setters (optional based on your needs)
@@ -36,5 +42,13 @@ public class HelperClass {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Object getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Object timestamp) {
+        this.timestamp = timestamp;
     }
 }
